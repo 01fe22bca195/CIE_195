8444901d8669367f241fa63716d415f2c005f21a
Ranjita <01fe22bca195@kletech.ac.in>
2024-03-19 00:46:11 +0530

    Code related to Student Information

---
+++ Student.java
@@ -0,0 +1,23 @@
+class Student {
+    String name="Ranjita";
+    String program="BCA";
+    String sem="4th";
+    public String RegisteredCourse[]={"","C#","AS","Java"};
+
+    public void display(){
+        System.out.print("Student Details\n");
+        System.out.print("Name:"+name);
+        System.out.print("\nProgram:"+program);
+        System.out.print("\nSemester:"+sem);
+        System.out.print("\nRegistered Courses\n");
+        for(int i=1; i<=3; i++){
+            System.out.print(i+" "+RegisteredCourse[i]+"\n");
+        }
+    }
+
+    public static void main(String[] args){
+        Student s = new Student();
+        s.display();
+       
+    }
+}
